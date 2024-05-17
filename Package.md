d18a59530cd75e73d8f582c72bc810b088af052c
Nasss23 <103917238+Nasss23@users.noreply.github.com>
2024-05-17 20:06:17 +0700

    1.Những folders và packages cần thiết khi khởi tạo Project!

---
+++ Package.md
@@ -0,0 +1,7 @@
+## npm i express --save
+
+## npm i morgan --save-dev
+
+## npm i helmet --save-dev
+
+## npm i compression --save-dev
---
+++ server.js
@@ -0,0 +1,14 @@
+//Only used to start the server
+const app = require("./src/app");
+const PORT = 3000
+
+const server = app.listen(PORT, () => {
+    console.log(`WSV Ecommerce start with port ${PORT}`)
+})
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        console.log(`Exit server Express`)
+    })
+    // notify.send()
+})
