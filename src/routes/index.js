4156bf2003efd672b75acd287df1df52fa1e17a6
Nasss23 <103917238+Nasss23@users.noreply.github.com>
2024-05-19 15:37:31 +0700

    6.Custom Dynamic Middleware for ApiKey and Permissions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,17 @@
 const express = require('express')
+const { route } = require('../app')
+const { apiKey, permission } = require('../auth/checkAuth')
 const router = express.Router()
+
+// check apiKey
+router.use(apiKey)
+// check permission
+router.use(permission('0000'))
 
 
 router.use('/v1/api/', require('./access/index'))
 
-// router.get('/', (req, res, next) => {
-//     return res.status(200).json({
-//         message: 'Welcom'
-//     })
-// })
+
 
 
 module.exports = router
