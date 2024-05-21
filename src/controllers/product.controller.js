1825014c37a969bb3bc6884c7a786d0dcc5aab43
Nasss23 <103917238+Nasss23@users.noreply.github.com>
2024-05-21 21:11:19 +0700

    14.Api Service Products (Optimal For LV.xxx) | Nâng cao cấu trúc Product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,11 +3,13 @@ const { SuccessResponse } = require('../core/success.response')
 
 class ProductController {
     createProduct = async (req, res, next) => {
-
         new SuccessResponse({
             message: 'Success new create product',
-            metadata: await ProductService.createProduct(req.body.product_type, req.body)
-        })
+            metadata: await ProductService.createProduct(req.body.product_type, {
+                ...req.body,
+                product_shop: req.user.userId
+            })
+        }).send(res)
     }
 }
 
